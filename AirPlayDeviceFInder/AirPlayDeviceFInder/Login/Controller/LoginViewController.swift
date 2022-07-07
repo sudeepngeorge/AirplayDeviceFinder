2eed9e9dc07cc191308554867e77bf140feca31b
Sudeep George <sudeep.george@ust-global.com>
2022-07-07 09:43:49 +0530

    Created Xcode Project and Implemented Folder Structure - Removed scene delegate - Application appearnace restricted to light

---
+++ AirPlayDeviceFInder/AirPlayDeviceFInder/Login/Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirPlayDeviceFInder
+//
+//  Created by Sudeep George on 07/07/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.red
+    }
+
+
+}
+
